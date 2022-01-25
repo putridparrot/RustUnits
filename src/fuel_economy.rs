d2988d2cbb9b1c1c273b48edbbf797cf33111d4b
Mark Timmings <mark.timmings@mtbsoftware.co.uk>
2022-01-25 23:49:13 +0000

    Changes the text for LitresPer100Kilometre

--- src/fuel_economy.rs
+++ src/fuel_economy.rs
@@ -20,30 +20,30 @@ pub mod fuel_economy {
 		pub fn to_miles_per_gallon(value: f64) -> f64 {
 			return value * 2.82481;
 		}
-		/// Converts the supplied Kilometre Per Litre value to Litres per 100 Kilometres
+		/// Converts the supplied Kilometre Per Litre value to Litres Per 100 Kilometres
 		/// # Arguments
 		/// * `value` - The Kilometre Per Litre input value
-		pub fn to_litresper100_kilometres(value: f64) -> f64 {
+		pub fn to_litres_per100_kilometres(value: f64) -> f64 {
 			return 100.0 / value;
 		}
 	}
-	/// Litres per 100 Kilometres conversion functions
-	pub mod litresper100_kilometres {
-		/// Converts the supplied Litres per 100 Kilometres value to Kilometre Per Litre
+	/// Litres Per 100 Kilometres conversion functions
+	pub mod litres_per100_kilometres {
+		/// Converts the supplied Litres Per 100 Kilometres value to Kilometre Per Litre
 		/// # Arguments
-		/// * `value` - The Litres per 100 Kilometres input value
+		/// * `value` - The Litres Per 100 Kilometres input value
 		pub fn to_kilometre_per_litre(value: f64) -> f64 {
 			return 100.0 / value;
 		}
-		/// Converts the supplied Litres per 100 Kilometres value to Miles Per Gallon
+		/// Converts the supplied Litres Per 100 Kilometres value to Miles Per Gallon
 		/// # Arguments
-		/// * `value` - The Litres per 100 Kilometres input value
+		/// * `value` - The Litres Per 100 Kilometres input value
 		pub fn to_miles_per_gallon(value: f64) -> f64 {
 			return 282.481 / value;
 		}
-		/// Converts the supplied Litres per 100 Kilometres value to US Miles Per Gallon
+		/// Converts the supplied Litres Per 100 Kilometres value to US Miles Per Gallon
 		/// # Arguments
-		/// * `value` - The Litres per 100 Kilometres input value
+		/// * `value` - The Litres Per 100 Kilometres input value
 		pub fn to_u_s_miles_per_gallon(value: f64) -> f64 {
 			return 235.215 / value;
 		}
@@ -62,10 +62,10 @@ pub mod fuel_economy {
 		pub fn to_u_s_miles_per_gallon(value: f64) -> f64 {
 			return value / 1.20095;
 		}
-		/// Converts the supplied Miles Per Gallon value to Litres per 100 Kilometres
+		/// Converts the supplied Miles Per Gallon value to Litres Per 100 Kilometres
 		/// # Arguments
 		/// * `value` - The Miles Per Gallon input value
-		pub fn to_litresper100_kilometres(value: f64) -> f64 {
+		pub fn to_litres_per100_kilometres(value: f64) -> f64 {
 			return 282.481 / value;
 		}
 	}
@@ -83,10 +83,10 @@ pub mod fuel_economy {
 		pub fn to_miles_per_gallon(value: f64) -> f64 {
 			return value * 1.20095;
 		}
-		/// Converts the supplied US Miles Per Gallon value to Litres per 100 Kilometres
+		/// Converts the supplied US Miles Per Gallon value to Litres Per 100 Kilometres
 		/// # Arguments
 		/// * `value` - The US Miles Per Gallon input value
-		pub fn to_litresper100_kilometres(value: f64) -> f64 {
+		pub fn to_litres_per100_kilometres(value: f64) -> f64 {
 			return 235.215 / value;
 		}
 	}
@@ -141,85 +141,85 @@ mod tests {
 
 	/// Need to convert to parameterized tests
 	#[test]
-	fn it_convert_knownkilometre_per_litre_to_litresper100_kilometres_1() {
-		let result: f64 = fuel_economy::kilometre_per_litre::to_litresper100_kilometres(109.0);
+	fn it_convert_knownkilometre_per_litre_to_litres_per100_kilometres_1() {
+		let result: f64 = fuel_economy::kilometre_per_litre::to_litres_per100_kilometres(109.0);
 		assert_approx_eq!(0.917431, result, 0.01);
 	}
 
 	/// Need to convert to parameterized tests
 	#[test]
-	fn it_convert_knownkilometre_per_litre_to_litresper100_kilometres_2() {
-		let result: f64 = fuel_economy::kilometre_per_litre::to_litresper100_kilometres(0.8);
+	fn it_convert_knownkilometre_per_litre_to_litres_per100_kilometres_2() {
+		let result: f64 = fuel_economy::kilometre_per_litre::to_litres_per100_kilometres(0.8);
 		assert_approx_eq!(125.0, result, 0.01);
 	}
 
 	/// Need to convert to parameterized tests
 	#[test]
-	fn it_convert_knownkilometre_per_litre_to_litresper100_kilometres_3() {
-		let result: f64 = fuel_economy::kilometre_per_litre::to_litresper100_kilometres(2.3);
+	fn it_convert_knownkilometre_per_litre_to_litres_per100_kilometres_3() {
+		let result: f64 = fuel_economy::kilometre_per_litre::to_litres_per100_kilometres(2.3);
 		assert_approx_eq!(43.4783, result, 0.01);
 	}
 
 	/// Need to convert to parameterized tests
 	#[test]
-	fn it_convert_knownlitresper100_kilometres_to_kilometre_per_litre_1() {
-		let result: f64 = fuel_economy::litresper100_kilometres::to_kilometre_per_litre(0.7);
+	fn it_convert_knownlitres_per100_kilometres_to_kilometre_per_litre_1() {
+		let result: f64 = fuel_economy::litres_per100_kilometres::to_kilometre_per_litre(0.7);
 		assert_approx_eq!(142.857, result, 0.01);
 	}
 
 	/// Need to convert to parameterized tests
 	#[test]
-	fn it_convert_knownlitresper100_kilometres_to_kilometre_per_litre_2() {
-		let result: f64 = fuel_economy::litresper100_kilometres::to_kilometre_per_litre(109.0);
+	fn it_convert_knownlitres_per100_kilometres_to_kilometre_per_litre_2() {
+		let result: f64 = fuel_economy::litres_per100_kilometres::to_kilometre_per_litre(109.0);
 		assert_approx_eq!(0.917431, result, 0.01);
 	}
 
 	/// Need to convert to parameterized tests
 	#[test]
-	fn it_convert_knownlitresper100_kilometres_to_kilometre_per_litre_3() {
-		let result: f64 = fuel_economy::litresper100_kilometres::to_kilometre_per_litre(0.012);
+	fn it_convert_knownlitres_per100_kilometres_to_kilometre_per_litre_3() {
+		let result: f64 = fuel_economy::litres_per100_kilometres::to_kilometre_per_litre(0.012);
 		assert_approx_eq!(8333.3333, result, 0.01);
 	}
 
 	/// Need to convert to parameterized tests
 	#[test]
-	fn it_convert_knownlitresper100_kilometres_to_miles_per_gallon_1() {
-		let result: f64 = fuel_economy::litresper100_kilometres::to_miles_per_gallon(107.0);
+	fn it_convert_knownlitres_per100_kilometres_to_miles_per_gallon_1() {
+		let result: f64 = fuel_economy::litres_per100_kilometres::to_miles_per_gallon(107.0);
 		assert_approx_eq!(2.64001, result, 0.01);
 	}
 
 	/// Need to convert to parameterized tests
 	#[test]
-	fn it_convert_knownlitresper100_kilometres_to_miles_per_gallon_2() {
-		let result: f64 = fuel_economy::litresper100_kilometres::to_miles_per_gallon(0.8);
+	fn it_convert_knownlitres_per100_kilometres_to_miles_per_gallon_2() {
+		let result: f64 = fuel_economy::litres_per100_kilometres::to_miles_per_gallon(0.8);
 		assert_approx_eq!(353.101, result, 0.01);
 	}
 
 	/// Need to convert to parameterized tests
 	#[test]
-	fn it_convert_knownlitresper100_kilometres_to_miles_per_gallon_3() {
-		let result: f64 = fuel_economy::litresper100_kilometres::to_miles_per_gallon(0.02);
+	fn it_convert_knownlitres_per100_kilometres_to_miles_per_gallon_3() {
+		let result: f64 = fuel_economy::litres_per100_kilometres::to_miles_per_gallon(0.02);
 		assert_approx_eq!(14124.05, result, 0.01);
 	}
 
 	/// Need to convert to parameterized tests
 	#[test]
-	fn it_convert_knownlitresper100_kilometres_to_u_s_miles_per_gallon_1() {
-		let result: f64 = fuel_economy::litresper100_kilometres::to_u_s_miles_per_gallon(12.0);
+	fn it_convert_knownlitres_per100_kilometres_to_u_s_miles_per_gallon_1() {
+		let result: f64 = fuel_economy::litres_per100_kilometres::to_u_s_miles_per_gallon(12.0);
 		assert_approx_eq!(19.6012, result, 0.01);
 	}
 
 	/// Need to convert to parameterized tests
 	#[test]
-	fn it_convert_knownlitresper100_kilometres_to_u_s_miles_per_gallon_2() {
-		let result: f64 = fuel_economy::litresper100_kilometres::to_u_s_miles_per_gallon(0.3);
+	fn it_convert_knownlitres_per100_kilometres_to_u_s_miles_per_gallon_2() {
+		let result: f64 = fuel_economy::litres_per100_kilometres::to_u_s_miles_per_gallon(0.3);
 		assert_approx_eq!(784.049, result, 0.01);
 	}
 
 	/// Need to convert to parameterized tests
 	#[test]
-	fn it_convert_knownlitresper100_kilometres_to_u_s_miles_per_gallon_3() {
-		let result: f64 = fuel_economy::litresper100_kilometres::to_u_s_miles_per_gallon(1.5);
+	fn it_convert_knownlitres_per100_kilometres_to_u_s_miles_per_gallon_3() {
+		let result: f64 = fuel_economy::litres_per100_kilometres::to_u_s_miles_per_gallon(1.5);
 		assert_approx_eq!(156.81, result, 0.01);
 	}
 
@@ -267,22 +267,22 @@ mod tests {
 
 	/// Need to convert to parameterized tests
 	#[test]
-	fn it_convert_knownmiles_per_gallon_to_litresper100_kilometres_1() {
-		let result: f64 = fuel_economy::miles_per_gallon::to_litresper100_kilometres(22.3);
+	fn it_convert_knownmiles_per_gallon_to_litres_per100_kilometres_1() {
+		let result: f64 = fuel_economy::miles_per_gallon::to_litres_per100_kilometres(22.3);
 		assert_approx_eq!(12.66731, result, 0.01);
 	}
 
 	/// Need to convert to parameterized tests
 	#[test]
-	fn it_convert_knownmiles_per_gallon_to_litresper100_kilometres_2() {
-		let result: f64 = fuel_economy::miles_per_gallon::to_litresper100_kilometres(0.4);
+	fn it_convert_knownmiles_per_gallon_to_litres_per100_kilometres_2() {
+		let result: f64 = fuel_economy::miles_per_gallon::to_litres_per100_kilometres(0.4);
 		assert_approx_eq!(706.202, result, 0.01);
 	}
 
 	/// Need to convert to parameterized tests
 	#[test]
-	fn it_convert_knownmiles_per_gallon_to_litresper100_kilometres_3() {
-		let result: f64 = fuel_economy::miles_per_gallon::to_litresper100_kilometres(5.2);
+	fn it_convert_knownmiles_per_gallon_to_litres_per100_kilometres_3() {
+		let result: f64 = fuel_economy::miles_per_gallon::to_litres_per100_kilometres(5.2);
 		assert_approx_eq!(54.3233, result, 0.01);
 	}
 
@@ -330,22 +330,22 @@ mod tests {
 
 	/// Need to convert to parameterized tests
 	#[test]
-	fn it_convert_knownu_s_miles_per_gallon_to_litresper100_kilometres_1() {
-		let result: f64 = fuel_economy::u_s_miles_per_gallon::to_litresper100_kilometres(111.0);
+	fn it_convert_knownu_s_miles_per_gallon_to_litres_per100_kilometres_1() {
+		let result: f64 = fuel_economy::u_s_miles_per_gallon::to_litres_per100_kilometres(111.0);
 		assert_approx_eq!(2.11905, result, 0.01);
 	}
 
 	/// Need to convert to parameterized tests
 	#[test]
-	fn it_convert_knownu_s_miles_per_gallon_to_litresper100_kilometres_2() {
-		let result: f64 = fuel_economy::u_s_miles_per_gallon::to_litresper100_kilometres(0.4);
+	fn it_convert_knownu_s_miles_per_gallon_to_litres_per100_kilometres_2() {
+		let result: f64 = fuel_economy::u_s_miles_per_gallon::to_litres_per100_kilometres(0.4);
 		assert_approx_eq!(588.036, result, 0.01);
 	}
 
 	/// Need to convert to parameterized tests
 	#[test]
-	fn it_convert_knownu_s_miles_per_gallon_to_litresper100_kilometres_3() {
-		let result: f64 = fuel_economy::u_s_miles_per_gallon::to_litresper100_kilometres(5.2);
+	fn it_convert_knownu_s_miles_per_gallon_to_litres_per100_kilometres_3() {
+		let result: f64 = fuel_economy::u_s_miles_per_gallon::to_litres_per100_kilometres(5.2);
 		assert_approx_eq!(45.2336, result, 0.01);
 	}
 
